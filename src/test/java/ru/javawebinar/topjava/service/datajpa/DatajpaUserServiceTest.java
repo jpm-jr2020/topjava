9969f7bd006676687f38e32e3d76e92d652d9128
John PM <junks@yandex.ru>
2020-03-11 14:20:19 +0300

    HW5, Optional: review 2

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DatajpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DatajpaUserServiceTest.java
@@ -1,9 +1,10 @@
-package ru.javawebinar.topjava.service;
+package ru.javawebinar.topjava.service.datajpa;
 
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import java.util.Collections;
 

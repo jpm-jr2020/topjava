fd8e3a679019fc030654ed023cdb613b7071b2c7
John PM <junks@yandex.ru>
2020-03-25 16:24:02 +0300

    HW6, review 2

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.repository.JpaUtil;
 
-public class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
     @Autowired
     protected JpaUtil jpaUtil;
 
